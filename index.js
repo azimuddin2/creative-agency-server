40ade701c997384e3bbc67c76433828ce958f822
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-02-02 09:56:02 +0600

    database connection and services collection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vqxo9wk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        const servicesCollection = client.db('creativeAgency').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const services = await servicesCollection.find(query).toArray();
+            res.send(services);
+        });
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Creative Agency Server Running');
 });
 
 app.listen(port, () => {
-    console.log(`Creative agency server app listening on port ${port}`)
+    console.log(`Creative agency app listening on port ${port}`)
 });
