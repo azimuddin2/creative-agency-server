d2c66cb3f6314d4d5a6d3c44e9178bac8976dd68
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-02-12 12:28:32 +0600

    service delete api

--- index.js
+++ index.js
@@ -137,6 +137,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/services/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         // orders related api
         app.post('/orders', async (req, res) => {
